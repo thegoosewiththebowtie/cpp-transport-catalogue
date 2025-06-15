3528498060a37f3c633e9c8a9144641358ec038a
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-15 17:29:38 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,29 @@
-// место для вашего кода
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "input_reader.h"
+#include "stat_reader.h"
+
+int main() {
+    TransportCatalogue catalogue;
+    int base_request_count;
+    std::cin >> base_request_count >> std::ws;
+
+    {
+        InputReader reader;
+        for (int i = 0; i < base_request_count; ++i) {
+            std::string line;
+            getline(std::cin, line);
+            reader.ParseLine(line);
+        }
+        reader.ApplyCommands(catalogue);
+    }
+
+    int stat_request_count;
+    std::cin >> stat_request_count >> std::ws;
+    for (int i = 0; i < stat_request_count; ++i) {
+        std::string line;
+        getline(std::cin, line);
+        ParseAndPrintStat(catalogue, line, std::cout);
+    }
+}
