8da9cecc309cf9549ffb40d568a0c3a64db69a36
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-15 17:27:21 +0300

    Update input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,1 +1,95 @@
-// место для вашего кода
+#include "input_reader.h"
+
+#include <algorithm>
+#include <cassert>
+#include <iterator>
+
+Coordinates ParseCoordinates(std::string_view str) {
+    static const double nan = std::nan("");
+
+    std::size_t not_space = str.find_first_not_of(' ');
+    std::size_t comma = str.find(',');
+
+    if (comma == std::string_view::npos) {
+        return {nan, nan};
+    }
+
+    auto not_space2 = str.find_first_not_of(' ', comma + 1);
+
+    double lat = std::stod(std::string(str.substr(not_space, comma - not_space)));
+    double lng = std::stod(std::string(str.substr(not_space2)));
+
+    return {lat, lng};
+}
+
+std::string_view Trim(std::string_view string) {
+    const std::size_t start = string.find_first_not_of(' ');
+    if (start == std::string_view::npos) {
+        return {};
+    }
+    return string.substr(start, string.find_last_not_of(' ') + 1 - start);
+}
+
+std::vector<std::string_view> Split(std::string_view string, char delim) {
+    std::vector<std::string_view> result;
+
+    size_t pos = 0;
+    while ((pos = string.find_first_not_of(' ', pos)) < string.length()) {
+        std::size_t delim_pos = string.find(delim, pos);
+        if (delim_pos == std::string_view::npos) {
+            delim_pos = string.size();
+        }
+        if (std::string_view substr = Trim(string.substr(pos, delim_pos - pos)); !substr.empty()) {
+            result.push_back(substr);
+        }
+        pos = delim_pos + 1;
+    }
+    return result;
+}
+
+std::vector<std::string_view> ParseRoute(std::string_view route) {
+    if (route.find('>') != std::string_view::npos) {
+        return Split(route, '>');
+    }
+    auto stops = Split(route, '-');
+    std::vector<std::string_view> results(stops.begin(), stops.end());
+    results.insert(results.end(), std::next(stops.rbegin()), stops.rend());
+    return results;
+}
+
+CommandDescription ParseCommandDescription(std::string_view line) {
+    std::size_t colon_pos = line.find(':');
+    if (colon_pos == std::string_view::npos) {
+        return {};
+    }
+    std::size_t space_pos = line.find(' ');
+    if (space_pos >= colon_pos) {
+        return {};
+    }
+    std::size_t not_space = line.find_first_not_of(' ', space_pos);
+    if (not_space >= colon_pos) {
+        return {};
+    }
+    return {std::string(line.substr(0, space_pos)),
+            std::string(line.substr(not_space, colon_pos - not_space)),
+            std::string(line.substr(colon_pos + 1))};
+}
+
+void InputReader::ParseLine(std::string_view line) {
+    if (CommandDescription command_description = ParseCommandDescription(line)) {
+        commands_.push_back(std::move(command_description));
+    }
+}
+
+void InputReader::ApplyCommands([[maybe_unused]] TransportCatalogue& catalogue) const {
+    for (const CommandDescription &cmd: commands_) {
+        if (cmd.command == "Stop") {
+            Coordinates coords = ParseCoordinates(cmd.description);
+            catalogue.AddStop(cmd.id, coords);
+        } else if (cmd.command == "Bus") {
+            bool is_roundtrip = cmd.description.contains('>');
+            auto stops = ParseRoute(cmd.description);
+            catalogue.AddBus(cmd.id, stops, is_roundtrip);
+        }
+    }
+}
