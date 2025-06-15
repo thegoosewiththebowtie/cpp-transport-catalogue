e09637077440cb7f32f913ca6e4144f06a255953
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-15 17:28:28 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,29 @@
-// место для вашего кода
+#pragma once
+#include <string>
+#include <string_view>
+#include <vector>
+#include "geo.h"
+#include "transport_catalogue.h"
+
+struct CommandDescription {
+    explicit operator bool() const {
+        return !command.empty();
+    }
+
+    bool operator!() const {
+        return !operator bool();
+    }
+    
+    std::string command;      // Название команды
+    std::string id;           // id маршрута или остановки
+    std::string description;  // Параметры команды
+};
+
+class InputReader {
+public:
+    void ParseLine(std::string_view line);
+    void ApplyCommands(TransportCatalogue& catalogue) const;
+
+private:
+    std::vector<CommandDescription> commands_;
+};
