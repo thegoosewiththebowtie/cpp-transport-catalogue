446dd5a6dbd4eb7bbfd6a23dc5816e51ab1e416f
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-15 17:31:20 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,40 @@
-// место для вашего кода
+#pragma once
+#include <deque>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "geo.h"
+
+///дико извиняюсь за такое отставание, у меня в жизни ка*пец был, но я нагоню честно-честно, я теперь безработная, буду 24/7 кодить хыхы
+
+struct Bus;
+
+struct Stop {
+	std::string name;
+	Coordinates coordinates;
+};
+struct Bus {
+	std::string name;
+	std::vector<const Stop*> stops;
+	bool is_roundtrip;
+};
+
+class TransportCatalogue {
+public:
+	void AddStop(std::string_view name, Coordinates coords);
+	void AddBus(std::string_view name, const std::vector<std::string_view>& stop_names, bool is_roundtrip);
+
+	const Stop* FindStop(std::string_view name) const;
+	const Bus* FindBus(std::string_view name) const;
+
+	size_t GetBusStopCount(std::string_view bus_name) const;
+	size_t GetUniqueStopCount(std::string_view bus_name) const;
+
+	const std::unordered_map<std::string, const Bus*>* GetBusList (std::string_view stop_name) const;
+
+private:
+	std::unordered_map<std::string, std::unordered_map<std::string, const Bus*>> buses_by_stop;
+	std::unordered_map<std::string, Stop> stops_;
+	std::unordered_map<std::string, Bus> buses_;
+};
