18beacb8f59e7a1ac468455c71400008e81e8c9a
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-19 23:32:59 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,56 +1,91 @@
 #include "transport_catalogue.h"
+
+#include <iomanip>
 #include <iostream>
+#include <memory>
 #include <unordered_set>
 
-void TransportCatalogue::AddStop(std::string_view name, Coordinates coords) {
+void TransportCatalogue::AddStop(const std::string_view name, const Coordinates& coords) {
+    ///try_emplace тут ломает все, остановки же создаются при создании
+    ///автобусов, чтобы потом в автобус можно было добавить указатели,
+    ///а тут оно либо создается, либо меняется, а не только создается
     stops_[std::string(name)] = std::move(Stop{std::string(name), coords});
 }
 
-void TransportCatalogue::AddBus(std::string_view name, const std::vector<std::string_view>& stop_names, bool is_roundtrip) {
-    const std::string name_string = std::string(name);
+void TransportCatalogue::AddBus(const std::string_view name, const std::vector<std::string_view>& stop_names, const bool is_roundtrip) {
+    std::string name_string = std::string(name);
     std::vector<const Stop*> stop_ptrs;
-    buses_[name_string];
     for (auto stop_name : stop_names) {
         std::string stop_name_string = std::string(stop_name);
-        buses_by_stop[stop_name_string][name_string] = (FindBus(name));
+        buses_by_stop_[stop_name_string].insert(name_string);
         const Stop *it = &stops_[stop_name_string];;
         stop_ptrs.push_back(it);
     }
-    buses_[name_string]= std::move(Bus(name_string,stop_ptrs, is_roundtrip));
+    buses_.try_emplace(name_string, std::move(name_string), std::move(stop_ptrs), is_roundtrip);
 }
 
-const Stop* TransportCatalogue::FindStop(std::string_view name) const {
-    const std::string name_string = std::string(name);
-    return stops_.contains(name_string) ? &stops_.at(name_string) : nullptr;
+
+template <typename Container>
+auto FindTemplate(const Container& container, const std::string_view name) -> decltype(&container.begin()->second) {
+    auto iter = container.find(std::string(name));
+    return iter == container.end() ? nullptr : &iter->second;
 }
 
-const Bus* TransportCatalogue::FindBus(std::string_view name) const {
-    const std::string name_string = std::string(name);
-    return buses_.contains(name_string)? &buses_.at(name_string) : nullptr;
+const Stop* TransportCatalogue::FindStop(const std::string_view name) const {
+   return FindTemplate(stops_, name);
 }
 
-size_t TransportCatalogue::GetBusStopCount(std::string_view bus_name) const {
-    const Bus* bus = FindBus(bus_name);
+const Bus* TransportCatalogue::FindBus(const std::string_view name) const {
+    return FindTemplate(buses_, name);
+}
+
+const std::set<std::string> *TransportCatalogue::FindStopBusList(const std::string_view name) const {
+    return FindTemplate(buses_by_stop_, name);
+}
+
+size_t TransportCatalogue::GetBusStopCount(const std::string_view name) const {
+    const Bus* bus = FindBus(name);
     return bus ? bus->stops.size() : 0;
 }
 
-size_t TransportCatalogue::GetUniqueStopCount(std::string_view bus_name) const {
-    const Bus* bus = FindBus(bus_name);
+std::unique_ptr<std::tuple<size_t, size_t, std::string>> TransportCatalogue::GetBusStats(const Bus *bus) const {
+    double distance = 0;
+    if (!bus->stops.empty()) {
+        const Stop *prevstop = bus->stops.front();
+        for (const Stop *stop: bus->stops) {
+            distance+= ComputeDistance(prevstop->coordinates, stop->coordinates);
+            prevstop = stop;
+        }
+    }
+    std::ostringstream oss;
+    oss << std::setprecision(6) << distance;
+    return std::make_unique<std::tuple<size_t, size_t, std::string>>(
+        std::tuple{GetBusStopCount(bus->name), GetUniqueStopCount(bus->name), oss.str()});
+}
+
+size_t TransportCatalogue::GetUniqueStopCount(const std::string_view name) const {
+    const Bus* bus = FindBus(name);
     if (!bus) {
         return 0;
     }
-    std::unordered_map<std::string_view, Coordinates> unique_stops;
+    std::unordered_set<std::string_view> unique_stops;
     for (const Stop* stop : bus->stops) {
-        if (unique_stops[stop->name] == stop->coordinates) {
-            continue;
-        }
-        unique_stops[stop->name] = stop->coordinates;
+        unique_stops.insert(stop->name);
     }
     return unique_stops.size();
 }
 
-const std::unordered_map<std::string, const Bus *>* TransportCatalogue::GetBusList(std::string_view stop_name) const {
-    return buses_by_stop.contains(std::string(stop_name))? &buses_by_stop.at(std::string(stop_name)) : nullptr;
+std::string TransportCatalogue::GetBusListAsString(const std::string_view name) const {
+    const std::set<std::string>* buslist = FindStopBusList(name);
+    std::string out;
+    if (buslist == nullptr) { return std::move(out);}
+    std::ostringstream ous;
+    for (const std::string& bus: *buslist) {
+        ous << bus << " ";
+    }
+    out = ous.str();
+    out.pop_back();
+    return std::move(out);
 }
 
 
