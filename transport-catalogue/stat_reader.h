27d8f1280ea5b76235d40ae3b32cec9ee9bec798
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-20 02:09:21 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -4,6 +4,12 @@
 #include <string_view>
 
 #include "transport_catalogue.h"
+class Parcer {
+public:
+    static void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output);
+private:
+    static std::string BusListToString(const std::set<std::string>* buslist);
 
-void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request,
-                       std::ostream& output);
+    template<typename T, typename = std::enable_if_t<std::is_floating_point_v<T>>>
+    static std::string FloatToString(T num, int precision);
+};
