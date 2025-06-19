ad94cfa642d4b83270f2f41c91e49e6b7c6856c6
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-19 23:31:04 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -2,7 +2,6 @@
 #include <string>
 #include <string_view>
 #include <vector>
-#include "geo.h"
 #include "transport_catalogue.h"
 
 struct CommandDescription {
@@ -13,7 +12,7 @@ struct CommandDescription {
     bool operator!() const {
         return !operator bool();
     }
-    
+
     std::string command;      // Название команды
     std::string id;           // id маршрута или остановки
     std::string description;  // Параметры команды
@@ -22,7 +21,13 @@ struct CommandDescription {
 class InputReader {
 public:
     void ParseLine(std::string_view line);
+
+    void ReadBaseInput(std::istream &ins, TransportCatalogue &transport_catalogue);
+
+    void ReadStatInput(std::istream &ins, std::ostream &ous, const TransportCatalogue &transport_catalogue);
+
     void ApplyCommands(TransportCatalogue& catalogue) const;
+
 
 private:
     std::vector<CommandDescription> commands_;
