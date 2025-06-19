c7efe79e4996ac3f9d24835d8dc21d3125a43aa5
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-19 23:31:42 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -5,50 +5,25 @@
 #include <iomanip>
 #include <iostream>
 
-void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request,
+void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request,
                        std::ostream& output) {
     auto space_pos = request.find(' ');
-    std::string_view type = request.substr(0, space_pos);
-    std::string_view id = request.substr(space_pos + 1);
+    const std::string_view type = request.substr(0, space_pos);
+    const std::string_view id = request.substr(space_pos + 1);
     if (type == "Bus") {
-        if (const auto* bus = tansport_catalogue.FindBus(id); !bus) {
+        if (const Bus *bus = transport_catalogue.FindBus(id); !bus) {
             output << "Bus " << id << ": not found\n";
         } else {
-            const size_t stop_count = tansport_catalogue.GetBusStopCount(id);
-            const size_t unique_stop_count = tansport_catalogue.GetUniqueStopCount(id);
-            double distance = 0;
-            if (!bus->stops.empty()) {
-                const Stop *prevstop = bus->stops.front();
-                for (const Stop *stop: bus->stops) {
-                    distance+= ComputeDistance(prevstop->coordinates, stop->coordinates);
-                    prevstop = stop;
-                }
-            }
-            std::ostringstream oss;
-            oss << std::setprecision(6) << distance;
-            output << "Bus " << id << ": " << stop_count << " stops on route, "
-                   << unique_stop_count << " unique stops, " << oss.str() << " route length\n";
+            std::unique_ptr<std::tuple<size_t, size_t, std::string>> bus_stats = transport_catalogue.GetBusStats(bus);
+            output << "Bus " << id << ": " << std::get<0>(*bus_stats) << " stops on route, "
+                   << std::get<1>(*bus_stats) << " unique stops, " << std::get<2>(*bus_stats) << " route length\n";
         }
     } else if (type == "Stop") {
-        auto getbuslist = [](const  std::unordered_map<std::string, const Bus*>& buslist) {
-            std::string out;
-            std::vector<std::string> busnames;
-            for (const std::pair<const std::string, const Bus *>& bus: buslist) {
-                busnames.push_back(bus.first);
-            }
-            std::sort(busnames.begin(), busnames.end());
-            for (std::string bus: busnames) {
-                out += std::move(bus) + " ";
-            }
-            out.pop_back();
-            return out;
-        };
-        if (const Stop *stop = tansport_catalogue.FindStop(id); !stop) {
-            output << "Stop " << id << ": not found\n";
-        } else {
-            output << "Stop " << id << (tansport_catalogue.GetBusList(stop->name) == nullptr ?
-                                        ": no buses\n" :
-                                        ": buses " + getbuslist(*tansport_catalogue.GetBusList(stop->name)) + "\n");
-        }
+        const Stop *stop = transport_catalogue.FindStop(id);
+        output << "Stop " << id << (!stop ?
+            ": not found\n"
+            : (transport_catalogue.FindStopBusList(stop->name) == nullptr ?
+            ": no buses\n"
+            : ": buses " + transport_catalogue.GetBusListAsString(stop->name) + "\n"));
     }
 }
