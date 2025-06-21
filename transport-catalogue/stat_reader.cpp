2e59ac7c23f9a93fafade4303df3d7370a16bd1b
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-21 03:30:42 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,56 +1,27 @@
 #include "stat_reader.h"
 
-#include <algorithm>
-#include <format>
-#include <iomanip>
-#include <iostream>
-
-void Parcer::ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request,
-                       std::ostream& output) {
-    auto space_pos = request.find(' ');
+void Parser::ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output) {
+    const std::size_t space_pos = request.find(' ');
     const std::string_view type = request.substr(0, space_pos);
     const std::string_view id = request.substr(space_pos + 1);
     if (type == "Bus") {
+        output << "Bus " << id << ": ";
         if (const Bus *bus = transport_catalogue.FindBus(id); !bus) {
-            output << "Bus " << id << ": not found\n";
+            output << "not found";
         } else {
-            std::tuple<size_t, size_t, double> bus_stats = TransportCatalogue::GetBusStats(bus);
-            output << "Bus " << id << ": " <<
-                std::get<0>(bus_stats)
-            << " stops on route, "
-            << std::get<1>(bus_stats)
-            << " unique stops, "
-            << FloatToString<double>(std::get<2>(bus_stats), 6)  << " route length\n";
+            output << BusStatsToStream{BusStats{bus, transport_catalogue}};
         }
     } else if (type == "Stop") {
-        const Stop *stop = transport_catalogue.FindStop(id);
-        if (!stop) {
-            output << "Stop " << id << ": not found\n";
+        output << "Stop " << id << ": ";
+        if (const Stop *stop = transport_catalogue.FindStop(id); !stop) {
+            output << "not found";
         } else {
-            if (auto buslist = transport_catalogue.FindStopBusList(stop->name); buslist == nullptr) {
-                output << "Stop " << id << ": no buses\n";
+            if (const std::set<std::string>* bus_list = transport_catalogue.FindStopBusList(stop->name); bus_list == nullptr) {
+                output << "no buses" ;
             } else {
-                output << "Stop " << id << ": buses " + BusListToString(buslist) + "\n";
+                output << BusListToStream{bus_list};
             }
         }
     }
-}
-
-template<typename T, typename>
-std::string Parcer::FloatToString(T num, const int precision){
-    std::ostringstream oss;
-    oss << std::setprecision(precision) << num;
-    return oss.str();
-}
-
-std::string Parcer::BusListToString(const std::set<std::string>* buslist) {
-    std::string out;
-    if (buslist == nullptr) { return std::move(out);}
-    std::ostringstream ous;
-    for (const std::string& bus: *buslist) {
-        ous << bus << " ";
-    }
-    out = ous.str();
-    out.pop_back();
-    return std::move(out);
+    output << "\n";
 }
