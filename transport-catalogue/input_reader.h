c09a834899727f953c809a7232c3714bea469d35
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-21 03:30:06 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -13,22 +13,26 @@ struct CommandDescription {
         return !operator bool();
     }
 
-    std::string command;      // Название команды
-    std::string id;           // id маршрута или остановки
-    std::string description;  // Параметры команды
+    std::string command;
+    std::string id;
+    std::string description;
 };
 
 class InputReader {
 public:
     void ParseLine(std::string_view line);
-
     void ReadBaseInput(std::istream &ins, TransportCatalogue &transport_catalogue);
-
-    void ReadStatInput(std::istream &ins, std::ostream &ous, const TransportCatalogue &transport_catalogue);
-
     void ApplyCommands(TransportCatalogue& catalogue) const;
 
+    static void ReadStatInput(std::istream &ins, std::ostream &ous, const TransportCatalogue &transport_catalogue);
 
 private:
+    static Geo::Coordinates ParseCoordinates(std::string_view str);
+    static std::string_view Trim(std::string_view string);
+    static std::vector<std::string_view> Split(std::string_view string, char delim);
+    static std::vector<std::string_view> ParseRoute(std::string_view route);
+    static CommandDescription ParseCommandDescription(std::string_view line);
+    static void ParseAndSetStopDistances(const std::string &stop_name, std::string_view description, TransportCatalogue &catalogue);
+
     std::vector<CommandDescription> commands_;
 };
