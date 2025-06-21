85abb364512669c50ccd097bef4ca09041c6a89e
The Goose <thegoosewiththebowtie@gmail.com>
2025-06-21 03:31:01 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -2,14 +2,43 @@
 
 #include <iosfwd>
 #include <string_view>
-
+#include <iomanip>
+#include <iostream>
 #include "transport_catalogue.h"
-class Parcer {
+
+class Parser {
 public:
     static void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output);
 private:
-    static std::string BusListToString(const std::set<std::string>* buslist);
-
-    template<typename T, typename = std::enable_if_t<std::is_floating_point_v<T>>>
-    static std::string FloatToString(T num, int precision);
+    struct BusStatsToStream {
+        const BusStats& bus_stats;
+        friend  std::ostream& operator<<(std::ostream& os, const BusStatsToStream& bsts) {
+            os  << bsts.bus_stats.stop_count
+                << " stops on route, "
+                << bsts.bus_stats.unique_stop_count
+                << " unique stops, "
+                << std::setprecision(6) << static_cast<double>(bsts.bus_stats.factual_distance)
+                << " route length, "
+                << bsts.bus_stats.curvature
+                << " curvature\n";
+            return os;
+        }
+    };
+    struct BusListToStream {
+        const std::set<std::string>* bus_list;
+        friend  std::ostream& operator<<(std::ostream& os, const BusListToStream& blts) {
+            if (!blts.bus_list) { return os; }
+            auto it = blts.bus_list->begin();
+            os << "buses " << *it;
+            ++it;
+            for (; it != blts.bus_list->end(); ++it) {
+                os << ' ' << *it;
+            }
+            return os;
+        }
+    };
 };
+
+
+
+
